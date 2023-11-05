d953446d938dfe74cca52493bb741c4d265b34e0
Patrik Dvořáček <patrik@dvoracek.dev>
2023-11-05 14:46:20 +0100

    feat(cleanup): 🗑️ Clean up the main branch.

--- src/components/button/Button.tsx
+++
@@ -1,7 +0,0 @@
-import { type ComponentPropsWithoutRef, type FC } from 'react';
-
-import styles from './Button.module.css';
-
-export const Button: FC<ComponentPropsWithoutRef<'button'>> = (properties) => {
-	return <button {...properties} className={styles['custom-button']} />;
-};
